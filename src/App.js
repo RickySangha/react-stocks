e80bb868c919ff6124f2fd355035ee2ec37f6a54
Ricky Sangha <sangha_ricky_07@hotmail.com>
2020-05-27 11:02:14 -0700

    Fix API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 const api = {
+  key: 'e63b8f285597e55b74d134459cbd51a7',
   incomeStatement:
-    "https://financialmodelingprep.com/api/v3/financials/income-statement/",
+    'https://financialmodelingprep.com/api/v3/financials/income-statement/',
   balanceSheet:
-    "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/",
-  dcf: "https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/",
-  profile: "https://financialmodelingprep.com/api/v3/company/profile/",
+    'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/',
+  dcf: 'https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/',
+  profile: 'https://financialmodelingprep.com/api/v3/company/profile/',
   cashFlow:
-    "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/",
+    'https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/',
 };
 
 const inflationRate = 3;
 const discountRate = 10;
 
 //Display numbers as currency (USD)
-const formatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
   minimumFractionDigits: 2,
 });
 
 function App() {
-  const [ticker, setTicker] = useState("");
+  const [ticker, setTicker] = useState('');
   const [stocks, setstocks] = useState([]);
   const [bond10y, setbond10y] = useState(2);
 
   //Make API request and setstocks after user hits enter on keyboard.
   function handleKey(e) {
-    if (e.key === "Enter") {
-      let dcfData = fetch(`${api.dcf}${ticker}`);
-      let incomeData = fetch(`${api.incomeStatement}${ticker}`);
-      let balanceData = fetch(`${api.balanceSheet}${ticker}`);
-      let profile = fetch(`${api.profile}${ticker}`);
-      let cashFlow = fetch(`${api.cashFlow}${ticker}`);
+    if (e.key === 'Enter') {
+      let dcfData = fetch(`${api.dcf}${ticker}/apikey=${api.key}`);
+      let incomeData = fetch(
+        `${api.incomeStatement}${ticker}/apikey=${api.key}`
+      );
+      let balanceData = fetch(`${api.balanceSheet}${ticker}/apikey=${api.key}`);
+      let profile = fetch(`${api.profile}${ticker}/apikey=${api.key}`);
+      let cashFlow = fetch(`${api.cashFlow}${ticker}/apikey=${api.key}`);
 
       Promise.all([dcfData, incomeData, balanceData, profile, cashFlow])
         .then((responses) => {
@@ -58,7 +61,7 @@ function App() {
           setstocks([...stocks, stock]);
         })
         .catch((err) => {
-          alert("Please enter a valid US stock ticker.");
+          alert('Please enter a valid US stock ticker.');
           console.log(err);
         });
     }
@@ -82,11 +85,11 @@ function App() {
         : stock.balanceSheet.financials.length + 1
     );
     let currentEquity = parseFloat(
-      balanceFinancials[0]["Total shareholders equity"]
+      balanceFinancials[0]['Total shareholders equity']
     );
     let previousEquity = parseFloat(
       balanceFinancials[balanceFinancials.length - 1][
-        "Total shareholders equity"
+        'Total shareholders equity'
       ]
     );
 
@@ -98,10 +101,10 @@ function App() {
 
     let bv =
       currentEquity /
-      stock.incomeStatement.financials[0]["Weighted Average Shs Out (Dil)"];
+      stock.incomeStatement.financials[0]['Weighted Average Shs Out (Dil)'];
 
     let dividend1yr = parseFloat(
-      stock.incomeStatement.financials[0]["Dividend per Share"]
+      stock.incomeStatement.financials[0]['Dividend per Share']
     );
 
     let fv =
@@ -113,7 +116,7 @@ function App() {
 
   //Calculate fair value with DCF model
   function calcDcf(stock) {
-    let fcf = parseFloat(stock.cashFlow.financials[0]["Free Cash Flow"]);
+    let fcf = parseFloat(stock.cashFlow.financials[0]['Free Cash Flow']);
     let dcf10 = 0;
 
     for (let i = 1; i < 11; i++) {
@@ -125,7 +128,7 @@ function App() {
     let dcfperp = fcf / (discountRate / 100 - inflationRate / 100);
     let dcfval =
       (dcf10 + dcfperp) /
-      stock.incomeStatement.financials[0]["Weighted Average Shs Out (Dil)"];
+      stock.incomeStatement.financials[0]['Weighted Average Shs Out (Dil)'];
     console.log(dcfval, dcfperp, dcf10);
     return dcfval;
   }
@@ -135,30 +138,30 @@ function App() {
     let symbol = stock.profile.symbol;
     let currentPrice = stock.profile.profile.price;
     let eps =
-      Math.round(stock.incomeStatement.financials[0]["EPS Diluted"] * 100) /
+      Math.round(stock.incomeStatement.financials[0]['EPS Diluted'] * 100) /
       100;
     let pe = Math.round((currentPrice / eps) * 100) / 100;
     let dcf =
-      typeof stock.profile.dcf === "number"
+      typeof stock.profile.dcf === 'number'
         ? formatter.format(
             (Math.round(stock.profile.dcf * 100) / 100).toFixed(2)
           )
-        : "N/A";
+        : 'N/A';
     let dividend = (
-      (stock.incomeStatement.financials[0]["Dividend per Share"] /
+      (stock.incomeStatement.financials[0]['Dividend per Share'] /
         currentPrice) *
       100
     ).toFixed(2);
     let netEquityGrowth5yr = stock.profile.netEquityGrowthRate;
     let netIncomes = [
       formatter.format(
-        stock.incomeStatement.financials[0]["Net Income"] / 1000000
+        stock.incomeStatement.financials[0]['Net Income'] / 1000000
       ),
       formatter.format(
-        stock.incomeStatement.financials[1]["Net Income"] / 1000000
+        stock.incomeStatement.financials[1]['Net Income'] / 1000000
       ),
       formatter.format(
-        stock.incomeStatement.financials[2]["Net Income"] / 1000000
+        stock.incomeStatement.financials[2]['Net Income'] / 1000000
       ),
     ];
     let fv = formatter.format(
@@ -178,7 +181,7 @@ function App() {
         <td>{netEquityGrowth5yr}%</td>
         <td>{fv}</td>
         <td>{dcf}</td>
-        <td className="delete" onClick={(e) => handleDelete(e)}>
+        <td className='delete' onClick={(e) => handleDelete(e)}>
           Delete
         </td>
       </tr>
@@ -186,26 +189,26 @@ function App() {
   });
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Intrinsic Value Calculator</h1>
       <input
-        type="text"
-        placeholder="Enter Ticker (eg. TSLA)"
-        className="searchbarticker"
+        type='text'
+        placeholder='Enter Ticker (eg. TSLA)'
+        className='searchbarticker'
         value={ticker}
         onChange={(e) => setTicker(e.target.value.toLocaleUpperCase())}
         onKeyPress={(e) => handleKey(e)}
       />
       <input
-        type="number"
-        placeholder="10yr Bond Rate (eg. 5%)"
-        className="searchbarbond"
+        type='number'
+        placeholder='10yr Bond Rate (eg. 5%)'
+        className='searchbarbond'
         onChange={(e) => setbond10y(parseFloat(e.target.value))}
         onKeyPress={(e) => handleKey(e)}
       />
       <table>
         <tbody>
-          <tr id="firstrow">
+          <tr id='firstrow'>
             <th>Stock</th>
             <th>Current Price</th>
             <th>EPS</th>
